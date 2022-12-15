9e5a39df360ff488372b69b0626f28d97aac2898
Vladimir Kostroba <vladimirkostroba22@gmail.com>
2022-12-15 22:56:18 +0200

    new commit

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,16 @@
 
+const str = 'я учу javascript!';
+
+const arr = str.split(' ');
+
+console.log(arr);
+let newArr = [];
+
+for(item of arr){
+    
+    newArr.push(item[0].toUpperCase()+item.slice(1));
+    
+}
+
+const newStr = newArr.join(' ');
+console.log(newStr);
